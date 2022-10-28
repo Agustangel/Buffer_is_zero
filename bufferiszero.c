8e0e21562df345a186c96e31b67ed97e27e671a0
Julia Melnikova <fox5rafy@gmail.com>
2022-10-28 20:42:17 +0300

    Fix nonzero_chunk

--- bufferiszero.c
+++ bufferiszero.c
@@ -29,8 +29,8 @@ static inline uint_fast32_t nonzero_chunk(const char *p)
 {
     uint_fast32_t tmp1, tmp2;    
 
-    tmp1 = load32(&tmp1);
-    tmp2 = load32(&tmp2);
+    tmp1 = load32(p);
+    tmp2 = load32(p + 4);
 
     return tmp1|tmp2;
 }
