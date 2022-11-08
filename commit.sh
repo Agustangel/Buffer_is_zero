70798ac926addbb6081ec28d46a1b1c2bd80fd6a
Julia Melnikova <fox5rafy@gmail.com>
2022-11-08 18:40:30 +0300

    Remove tmp commit message file after utilizing it
    =======================
    2376 cycles minimum for 4096 bytes
    1.72 bytes/cycle, 2.16 instructions/cycle

--- commit.sh
+++ commit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+TMP_FILENAME=commit_msg.tmp
+
+echo -e "Enter commit message:\n"
 read MESSAGE
 echo -e "$MESSAGE\n" > commit_msg.tmp
 echo "=======================" >> commit_msg.tmp
 ./biz.perf >> commit_msg.tmp
 git commit -F commit_msg.tmp
+rm commit_msg.tmp
 
