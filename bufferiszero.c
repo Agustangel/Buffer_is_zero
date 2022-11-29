b59febe3255911416953cd8959b95f6082a98618
Julia Melnikova <fox5rafy@gmail.com>
2022-11-29 18:52:49 +0300

    Fix error with incorrect idx + word_size check, fix error with size -1

--- bufferiszero.c
+++ bufferiszero.c
@@ -15,7 +15,7 @@ int buffer_is_zero(void* vbuf, size_t size)
     size_t last_word_pos = 0;
 
     // process until less than word_length bytes remain
-	for (unsigned long idx = 0; idx + word_length <= size; idx += word_length) 
+	for (unsigned long idx = 0; idx <= size; idx += word_length) 
     {
         memcpy(&chunk, buf + idx, sizeof(size_t));
         last_word_pos = idx;
@@ -24,7 +24,7 @@ int buffer_is_zero(void* vbuf, size_t size)
     }
     // process remaining bytes
     const char *current_byte = buf + last_word_pos;
-    const char *end = buf + size - 1;
+    const char *end = buf + size;
     unsigned char t = 0;
     do
         {
