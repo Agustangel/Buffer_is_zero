12316e3f3a4d0f58ca7513e62b83e739365ac9d6
Julia Melnikova <fox5rafy@gmail.com>
2022-12-12 19:23:36 +0300

    Unroll by 2, performance decrease

--- bufferiszero.c
+++ bufferiszero.c
@@ -12,12 +12,14 @@ int buffer_is_zero(void* vbuf, size_t size)
 	char* buf = (char*) vbuf;
     const size_t word_length = sizeof(size_t);
     size_t chunk = 0;
-    size_t last_chunk_pos = size  - size % (2 * word_length);
+    size_t last_chunk_pos = size  - size % (3 * word_length);
 
     int half_size = size / 2;
+    int third_size = size / 3;
     int double_word_length = 2 * word_length;
+    int triple_word_length = 3 * word_length;
     // process until less than word_length bytes remain
-	for (unsigned long idx = 0; idx + double_word_length <= half_size; idx += double_word_length) 
+	for (unsigned long idx = 0; idx + triple_word_length <= third_size; idx += triple_word_length) 
     {
         memcpy(&chunk, buf + idx, sizeof(size_t));
         if (chunk)
@@ -25,8 +27,12 @@ int buffer_is_zero(void* vbuf, size_t size)
         memcpy(&chunk, buf + idx + word_length, sizeof(size_t));
         if (chunk) 
             return 0;
+        memcpy(&chunk, buf + idx + 2 * word_length, sizeof(size_t));
+        if (chunk) 
+            return 0;
+            
     }
-    for (unsigned long idx = half_size; idx + double_word_length <= size; idx += double_word_length) 
+ 	for (unsigned long idx = third_size; idx + triple_word_length <= 2 * third_size; idx += triple_word_length) 
     {
         memcpy(&chunk, buf + idx, sizeof(size_t));
         if (chunk)
@@ -34,6 +40,23 @@ int buffer_is_zero(void* vbuf, size_t size)
         memcpy(&chunk, buf + idx + word_length, sizeof(size_t));
         if (chunk) 
             return 0;
+        memcpy(&chunk, buf + idx + 2 * word_length, sizeof(size_t));
+        if (chunk) 
+            return 0;
+            
+    }
+    	for (unsigned long idx = 2 * third_size; idx + triple_word_length <= size; idx += triple_word_length) 
+    {
+        memcpy(&chunk, buf + idx, sizeof(size_t));
+        if (chunk)
+            return 0;
+        memcpy(&chunk, buf + idx + word_length, sizeof(size_t));
+        if (chunk) 
+            return 0;
+        memcpy(&chunk, buf + idx + 2 * word_length, sizeof(size_t));
+        if (chunk) 
+            return 0;
+            
     }
     // process remaining bytes
     const char *current_byte = buf + last_chunk_pos;
